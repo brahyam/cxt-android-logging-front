09036978c00634249d66d89207446a88e7ed8645
Brahyam Meneses <meneses.brahyam@gmail.com>
2017-05-15 10:04:56 -0400

    changed ux to pen logs in new tab

--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Log} from '../log';
 import {LogService} from '../log.service';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'my-logs',
@@ -31,7 +32,23 @@ export class LogsComponent implements OnInit {
 
   onSelect(log:Log):void {
     this.selectedLog = log;
-    this.router.navigate(['/detail', this.selectedLog._id]);
+    // this.router.navigate(['/detail', this.selectedLog._id]);
+    // Navigate to log in new tab
+    window.open('/detail/' + log._id, '_blank');
+  }
+
+  onDownload(log:Log):void {
+    var now = new Date();
+    var blob = new Blob([log.logCat], {type: "text/plain;charset=utf-8"});
+    FileSaver.saveAs(blob, "LogCat-" + log.packageName + now.getTime() + ".txt");
+  }
+
+  onDelete(log:Log):void {
+    if (confirm('Are you sure you want to delete?')) {
+      this.logService.deleteLog(log._id).then(log => {
+        this.getLogs();
+      })
+    }
   }
 
   getLogs():void {
