d8678308ba889eca2f32480c943e4ae004e7a924
Brahyam Meneses <meneses.brahyam@gmail.com>
2017-06-22 17:16:01 -0400

    Updated backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://cxt-android-prod.herokuapp.com/'
+  apiBaseUrl: 'https://cxt-android-logging-backend.herokuapp.com/api/logs/'
 };
